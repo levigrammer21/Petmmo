f109023034b2c72056955f81b96c890e9ef78061
levigrammer21 <levigrammer@gmail.com>
2026-08-13 08:51:16 -0500

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAeConZ3nWkHyxPrSOUHcY3uEfB41Sf6PQ",
-  authDomain: "petmmo-158f7.firebaseapp.com",
-  projectId: "petmmo-158f7",
-  messagingSenderId: "793414581170",
-  appId: "1:793414581170:web:59bda27d5acd24ca4b42ab",
-};
-
-export const firebaseSdkVersion = "12.17.1";
